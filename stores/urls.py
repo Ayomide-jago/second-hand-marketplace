0ec4a80942d2319363db55398781856a8daf7fef
ODUGBESAN LEKE <Odugbesanayo8@gmail.com>
2025-06-20 10:32:09 +0100

    media serve for testing in production

--- stores/urls.py
+++ stores/urls.py
@@ -18,11 +18,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('marketstore/', include('marketstore.urls')),
 ]
 
+# this is right but just for presentation we would use otherwise
+'''
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+'''
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
